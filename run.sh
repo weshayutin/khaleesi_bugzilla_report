aee816a2526ec0f3b26be95bfd524499f9068cee
Wesley Hayutin <weshayutin@gmail.com>
2016-02-04 09:49:32 -0500

    force a unique list

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ source team
 
 pushd /tmp/; git clone https://github.com/redhat-openstack/khaleesi.git
 popd
-pushd /tmp/khaleesi/playbooks/; egrep -rn rhbz | sed 's/.*rhbz//' | cut -b 1-7 > /tmp/buglist
+pushd /tmp/khaleesi/playbooks/; egrep -rn rhbz | sed 's/.*rhbz//' | cut -b 1-7 | uniq > /tmp/buglist
 popd
 pwd
 python3 ospd_eng_bug_report.py
