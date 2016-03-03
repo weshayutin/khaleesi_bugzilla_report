c876f3c94252c9e801fda25807840d629174fe7d
Wesley Hayutin <weshayutin@gmail.com>
2016-03-03 14:38:30 -0500

    remove khaleesi dir before running report

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 source auth
 source team
 
+rm -Rf /tmp/khaleesi
 pushd /tmp/; git clone https://github.com/redhat-openstack/khaleesi.git
 popd
 pushd /tmp/khaleesi/playbooks/; egrep -rn rhbz | sed 's/.*rhbz//' | cut -b 1-7 | uniq > /tmp/buglist
