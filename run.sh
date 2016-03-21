27cff18828011ea082a3f383881da42724542dd7
Wesley Hayutin <weshayutin@gmail.com>
2016-03-21 08:50:54 -0400

    sort the list prior to uniq

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ source team
 rm -Rf /tmp/khaleesi
 pushd /tmp/; git clone https://github.com/redhat-openstack/khaleesi.git
 popd
-pushd /tmp/khaleesi/playbooks/; egrep -rn rhbz | sed 's/.*rhbz//' | cut -b 1-7 | uniq > /tmp/buglist
+pushd /tmp/khaleesi/playbooks/; egrep -rn rhbz | sed 's/.*rhbz//' | cut -b 1-7 | sort | uniq > /tmp/buglist
 popd
 pwd
 python3 ospd_eng_bug_report.py
